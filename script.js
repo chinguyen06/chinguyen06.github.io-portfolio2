34a2f17b82cdcf677e48a0a3553bf583dc1f6bfb
chinguyen06 <nguyenmaichi542006@gmail.com>
2026-03-02 20:00:58 +0700

    Refactor typing function to use typeEffect for roles

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-const text = "Data-Driven Financial Analyst";
-let i = 0;
-function typing(){
-  if(i < text.length){
-    document.getElementById("typing").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typing,60);
+const roles = [
+  "Data-Driven Financial Analyst",
+  "Quantitative Researcher",
+  "Investment Analyst",
+  "Green Finance Researcher"
+];
+
+const typingElement = document.getElementById("typing");
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+
+  const currentText = roles[roleIndex];
+
+  if (!isDeleting) {
+    // Typing
+    typingElement.textContent = currentText.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentText.length) {
+      setTimeout(() => isDeleting = true, 1200);
+    }
+
+  } else {
+    // Deleting
+    typingElement.textContent = currentText.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
   }
+
+  setTimeout(typeEffect, isDeleting ? 40 : 70);
 }
-typing();
+
+typeEffect();
